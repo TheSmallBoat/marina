ab4494f79b0b223b781af025a6cd7fa739bd1e83
TheSmallBoat <abe.chua@qq.com>
2020-07-07 18:38:53 +1200

    add sync.RWMutex

--- twins_pool.go
+++ twins_pool.go
@@ -9,6 +9,8 @@ import (
 type twinsPool struct {
 	m  map[string]*twin
 	sp sync.Pool
+
+	mu sync.RWMutex
 }
 
 func newTwinsPool() *twinsPool {
@@ -16,10 +18,16 @@ func newTwinsPool() *twinsPool {
 }
 
 func (tp *twinsPool) length() int {
+	tp.mu.RLock()
+	defer tp.mu.RUnlock()
+
 	return len(tp.m)
 }
 
 func (tp *twinsPool) exist(peerNodeId *kademlia.ID) (*twin, bool) {
+	tp.mu.RLock()
+	defer tp.mu.RUnlock()
+
 	t, exist := tp.m[peerNodeId.Pub.String()]
 	return t, exist
 }
@@ -31,7 +39,10 @@ func (tp *twinsPool) acquire(peerNodeId *kademlia.ID) *twin {
 		return t
 	}
 
+	tp.mu.RLock()
 	v := tp.sp.Get()
+	tp.mu.RUnlock()
+
 	if v == nil {
 		v = newTwin(peerNodeId)
 	} else {
@@ -39,14 +50,23 @@ func (tp *twinsPool) acquire(peerNodeId *kademlia.ID) *twin {
 	}
 
 	t = v.(*twin)
+
+	tp.mu.Lock()
 	tp.m[t.kadId.Pub.String()] = t
+	tp.mu.Unlock()
+
 	return t
 }
 
 func (tp *twinsPool) release(t *twin) {
+	tp.mu.RLock()
 	_, exist := tp.m[t.kadId.Pub.String()]
+	tp.mu.RUnlock()
+
 	if exist {
+		tp.mu.Lock()
 		delete(tp.m, t.kadId.Pub.String())
+		tp.mu.Unlock()
 	}
 
 	t.reset()
